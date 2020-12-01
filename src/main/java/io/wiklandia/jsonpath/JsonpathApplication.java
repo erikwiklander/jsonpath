3b57ba51793e1b6df7379a8c4ddc7e3abd861baa
Erik Wiklander <erik.wiklander@str8lines.com>
2020-11-30 22:07:06 -0600

    Initial commit

---
+++ src/main/java/io/wiklandia/jsonpath/JsonpathApplication.java
@@ -0,0 +1,13 @@
+package io.wiklandia.jsonpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonpathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonpathApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/wiklandia/jsonpath/JsonpathApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wiklandia.jsonpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonpathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
